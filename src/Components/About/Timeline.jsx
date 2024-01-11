5df7c26ce986ccee0018caee29ff650377577d58
Avinash-k-mishra <avinashkumarmishra247@gmail.com>
2024-01-11 17:31:16 +0530

    commit

---
+++ src/Components/About/Timeline.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+import "./Timeline.css";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+import WorkIcon from "@material-ui/icons/Work";
+import SchoolIcon from "@material-ui/icons/School";
+import StarRateIcon from "@material-ui/icons/StarRate";
+import { ThemeContext } from "../../Context/theme";
+
+export const Timeline = () => {
+  const [{ themename }] = React.useContext(ThemeContext);
+  const [linecolor, setlinecolor] = React.useState(
+    themename === "light" ? "#23283e" : "#fcfcfc"
+  );
+
+  React.useEffect(() => {
+    if (themename === "dark") {
+      setlinecolor("#fcfcfc");
+    } else {
+      setlinecolor("#23283e");
+    }
+  }, [themename]);
+  return (
+    <>
+      <div className="section mainsection">
+        <h2 className="section__title" data-aos="fade-right">
+          My <span className="different">Timeline 💫</span>
+        </h2>
+        <VerticalTimeline lineColor={linecolor}>
+
+        
+        
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date={"Jul 2020 - Aug 2024"}
+            contentStyle={{
+              boxShadow: `var(--shadow)`,
+              border: "3px solid var(--clr-primary)",
+              backgroundColor: `var(--clr-bg)`,
+              textAlign: "center",
+              color: `var(--clr-fg-alt)`,
+            }}
+            contentArrowStyle={{
+              borderRight: "16px solid  var(--clr-primary)",
+            }}
+            iconStyle={{
+              border: ` 3px solid var(--clr-primary)`,
+              backgroundColor: `var(--clr-bg)`,
+              color: `var(--clr-primary)`,
+            }}
+            icon={<SchoolIcon />}
+          >
+            <h3
+              className="vertical-timeline-element-title"
+              data-aos="fade-right"
+            >
+              Student [BE (Computer Engineering)]
+            </h3>
+            <h4
+              className="vertical-timeline-element-subtitle"
+              data-aos="fade-right"
+            >
+               Mumbai University, Thane (MH)
+            </h4>
+            <p data-aos="fade-right">
+              Studied Computer Engineering and Holding Top spot most of the time.
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date={"March 2019"}
+            contentStyle={{
+              boxShadow: `var(--shadow)`,
+              border: "3px solid var(--clr-primary)",
+              backgroundColor: `var(--clr-bg)`,
+              textAlign: "center",
+              color: `var(--clr-fg-alt)`,
+            }}
+            contentArrowStyle={{
+              borderRight: "16px solid  var(--clr-primary)",
+            }}
+            iconStyle={{
+              border: ` 3px solid var(--clr-primary)`,
+              backgroundColor: `var(--clr-bg)`,
+              color: `var(--clr-primary)`,
+            }}
+            icon={<SchoolIcon />}
+          >
+            <h3
+              className="vertical-timeline-element-title"
+              data-aos="fade-right"
+            >
+              Student [HSC (Science)]
+            </h3>
+            <h4
+              className="vertical-timeline-element-subtitle"
+              data-aos="fade-right"
+            >
+              R K Talreja College, Ulhasnagar, Maharashtra
+            </h4>
+            <p data-aos="fade-right">
+              Studied 12th Science and Passed with 62.48 Percentage.
+            </p>
+          </VerticalTimelineElement>
+
+
+        </VerticalTimeline>
+      </div>
+    </>
+  );
+};
